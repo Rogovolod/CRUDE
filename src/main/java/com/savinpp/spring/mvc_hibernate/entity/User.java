beefa3e026b7de8a93845de1ada187a39b1f3920
Rogovolod <Rogovolod@gmail.com>
2020-12-28 15:15:18 +0900

    html update

--- src/main/java/com/savinpp/spring/mvc_hibernate/entity/User.java
+++ src/main/java/com/savinpp/spring/mvc_hibernate/entity/User.java
@@ -24,21 +24,11 @@ public class User {
     @Column(name = "salary")
     private int salary;
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", department='" + department + '\'' +
-                ", salary=" + salary +
-                '}';
-    }
 
     public User() {
     }
 
-    public User(int id, String name, String surname, String department, int salary) {
+    public User(int id, String name, String surname, String department, int salary, int password, int login, int role) {
         this.id = id;
         this.name = name;
         this.surname = surname;
@@ -86,5 +76,14 @@ public class User {
         this.salary = salary;
     }
 
-
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
